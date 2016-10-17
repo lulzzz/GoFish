338a178ed164e5b2aa12a0ecd426a1a2422f5346
jakimber <j_a_kimber@hotmail.com>
2016-10-17 11:47:23 -0400

    Restructured Source

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-dotnet restore
-dotnet build ./**/project.json
-dotnet publish -c release ./GoFish.Advert
-dotnet publish -c release ./GoFish.Inventory
-dotnet publish -c release ./GoFish.Advert.Receiver
-dotnet publish -c release ./GoFish.Inventory.Receiver
+while [[ "$#" > 1 ]]; do case $1 in
+    -o) options="$2";;
+    *) break;;
+  esac; shift; shift
+done
+
+if [ "$options" = "publish" ]; then
+    echo "publish-only flag set"
+else
+    dotnet restore
+    dotnet build ./**/project.json
+fi
+
+dotnet publish -c release ./GoFish.Api/GoFish.Advert
+dotnet publish -c release ./GoFish.Api/GoFish.Inventory
+dotnet publish -c release ./GoFish.Api/GoFish.Advert.Receiver
+dotnet publish -c release ./GoFish.Api/GoFish.Inventory.Receiver
 dotnet publish -c release ./GoFish.Identity
-dotnet publish -c release ./GoFish.UI.MVC
+dotnet publish -c release ./GoFish.UI/GoFish.UI.MVC
